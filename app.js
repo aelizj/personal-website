01a5912d64a73eab331e238e114ce7233479a926
Anne Jones <anne1365@outlook.com>
2022-05-29 23:10:33 -0700

    Looking at routing

--- app.js
+++ app.js
@@ -5,10 +5,3 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.listen(port, () => console.log('Running your app!'));
-
-app.get('/', (req, res) => {
-  res.send('GET request to annejones.dev/')
-  let para = document.createElement('h1');
-  para.textContent = "HELLO WORLD"
-  document.body.appendChild('para');
-} )
